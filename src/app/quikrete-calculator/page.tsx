5845e067afac8e4a177abf26670dd6eb8a3af530
qinjunkun <hiyefu@gmail.com>
2026-06-10 19:09:45 +0800

    style optimize

--- src/app/quikrete-calculator/page.tsx
+++ src/app/quikrete-calculator/page.tsx
@@ -54,7 +54,7 @@ export default function QuikreteCalculatorPage() {
 
       {/* Quikrete Calculator Section */}
       <section className="container mx-auto px-4 py-8">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <QuikreteCalculator />
         </div>
       </section>

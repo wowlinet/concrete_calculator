4db290278d86e6e7f95c006c5ca0b7cdebf81c83
qinjunkun <hiyefu@gmail.com>
2026-06-11 15:45:59 +0800

    weight calculator optimize

--- src/app/weight-calculator/page.tsx
+++ src/app/weight-calculator/page.tsx
@@ -54,7 +54,7 @@ export default function WeightCalculatorPage() {
 
       {/* Weight Calculator Section */}
       <section className="container mx-auto px-4 py-8">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <WeightCalculator />
         </div>
       </section>
